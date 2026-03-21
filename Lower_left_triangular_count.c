d616dc4e24b77442dc0bb9a1e79d194ee86330fd
Inakshi <inakshi.8734@gmail.com>
2026-03-21 15:12:16 +0530

    Counts how many elements in the lower left triangle are greater than a target value x.

---
+++ Lower_left_triangular_count.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main(){
+    int n , m;
+    printf("enter the row and column of the array :");
+    scanf("%d%d",&n,&m);
+    int x;
+    printf("enter the X :");
+    scanf("%d",&x);
+    int a[n][m];
+    int i,j,c=0;
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            printf("enter your element a[%d][%d] :",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("\narray -\n");
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+         printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    if(n==m){
+        for(j=n-1;j>=0;j--){
+            for(i=j;i<n;i++){
+                if(a[i][j]>x) c++;
+            }
+        }
+    }
+    else printf("not a square matrix !");
+    printf("\ncount = %d",c);
+    return 0;
+}
