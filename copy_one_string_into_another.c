f7af4ba7487633e784d9d125b0ce28d367efdb72
Inakshi <inakshi.8734@gmail.com>
2026-03-25 23:20:29 +0530

    program to copy one string into another string.

---
+++ copy_one_string_into_another.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char s1[100], s2[100];
+    int i;
+    printf("enter the source string: ");
+    scanf("%s", s1);
+    for (i = 0; s1[i] != '\0'; i++) {
+        s2[i] = s1[i];
+    }
+    s2[i] = '\0';
+    printf("original string: %s\n", s1);
+    printf("copied string  : %s\n", s2);
+    return 0;
+}
