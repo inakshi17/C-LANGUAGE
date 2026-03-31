0add7f9a2f44a498e7f785cc6608ee1ed0f7d613
Inakshi <inakshi.8734@gmail.com>
2026-03-31 22:26:32 +0530

    Implement Heron's formula to calculate triangle area.

---
+++ Area_of_triangle_Heron's_Formula.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a, b, c, s, area;
+    printf("Enter lengths of three sides: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+    s = (a + b + c) / 2;
+    area = sqrt(s * (s - a) * (s - b) * (s - c));
+    printf("Area of the triangle = %.2lf\n", area);
+    return 0;
+}
