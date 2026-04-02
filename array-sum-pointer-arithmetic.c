fab060a32920d0c7f3ab92be42fd1ede3764ef6d
Inakshi <inakshi.8734@gmail.com>
2026-04-02 23:13:17 +0530

    Add array sum program with pointer arithmetic.
    Implement array input and sum using pointer arithmetic

---
+++ array-sum-pointer-arithmetic.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("enter the number of element :");
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        printf("enter element %d:",i+1);
+        scanf("%d",&a[i]);
+    }
+    printf("array-\n");
+    for(int i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+    int s=0;
+    int *A;
+    A=&a[0];
+    for(int i=0;i<n;i++){
+        s=s+*A;
+        printf("\nelement - %d\naddress - %u\n",*A,A);
+        A++;
+    }
+    printf("\nsum of all elemnet of array - %d",s);
+    return 0;
+}
