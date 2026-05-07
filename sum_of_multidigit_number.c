d5d4ce5401cefdb6f67edf58fca1b871520e2707
Inakshi <inakshi.8734@gmail.com>
2026-05-07 22:32:00 +0530

    Add program to sum digits of multidigit numbers
    This program takes multiple multidigit numbers as input, sums the digits of each number, and outputs the new array of summed digits.

---
+++ sum_of_multidigit_number.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+    int n, i;
+    printf("enter number of element : ");
+    scanf("%d",&n);
+    int *a;
+    a=(int *)malloc(n*sizeof(int));
+    for(i=0;i<n;i++){
+        printf("enter element %d : ",i+1);
+        scanf("%d",&a[i]);
+    }
+    printf("\narray - ");
+    for(i=0;i<n;i++){
+        int s=0;
+        printf("%d ",a[i]);
+        if(a[i]/10 != 0){
+            while(a[i]>0){
+                s=s+(a[i]%10);
+                a[i]=a[i]/10;
+            }
+            a[i]=s;
+        }
+    }
+    printf("\nnew array - ");
+    for(i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+    free(a);
+    return 0;
+}
