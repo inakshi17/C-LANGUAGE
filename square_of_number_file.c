acf0feb84caf188cbe27d582a31c1c7a4d1ee005
Inakshi <inakshi.8734@gmail.com>
2026-05-08 16:57:22 +0530

    Implement square calculation from input file.
    Reads integers from 'numbers.txt', calculates their squares, and writes the results to 'square.txt'. Displays content of both input and output files.

---
+++ square_of_number_file.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+    FILE *inp,*out;
+    inp = fopen("numbers.txt","r");
+    out = fopen("square.txt", "w");
+    int x;
+    printf("\ncontent of input file - ");
+    while(fscanf(inp,"%d", &x) != EOF){
+        fprintf(out, "%d ", x*x);
+        printf("%d ",x);
+    }
+    fclose(inp);
+    fclose(out);
+    out = fopen("square.txt","r");
+    printf("\ncontent of output file - ");
+    while(fscanf(out, "%d", &x) != EOF){
+        printf("%d ",x);
+    }
+    fclose(out);
+    return 0;
+}
